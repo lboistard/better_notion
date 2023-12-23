5264e6b5d6fa3ebdd4531ba23aa5139e6e159e0a
Lucas Boistard <49691983+lboistard@users.noreply.github.com>
2023-12-23 22:35:23 +0100

    auth layout left side

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,26 +1,13 @@
-
-import { CommandLineIcon } from "@heroicons/react/24/solid";
+import AuthLayout from "@/layouts/AuthLayout";
 
 const Login = () => {
   return (
-    <div className="columns-2">
-      {/* Left */}
-      <div className="container w-full h-screen">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6">
-          <div className="flex text-center ">
-            <CommandLineIcon className="h-8 w-8" />
-            <h1 className="text-2xl font-semibold">
-              Better Notion
-            </h1>
-          </div>
-        </div>
-      </div>
-
-      {/* Right */}
+    <AuthLayout>
+      {/* Right - Form */}
       <div className="container w-full h-screen">
         form
       </div>
-    </div>
+    </AuthLayout>
   )
 }
 export default Login;
--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { CommandLineIcon } from "@heroicons/react/24/solid";
+import image from "../resources/images/BackgroundAuth.png";
 
 interface AuthLayoutProps {
   children: React.ReactNode
@@ -6,12 +8,27 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div style={{
-      display: "flex",
-    }}>
-      <div style={{
-        width: "40%",
-      }}>bannier</div>
+    <div className="columns-2">
+      {/* Left */}
+      <div className="container w-full h-screen bg-fixed" style={{ backgroundImage:`url(${image})` }}>
+        <div className="mx-auto flex w-full h-full flex-col justify-center space-y-6">
+
+          <div className="flex text-center justify-center align-center">
+            <CommandLineIcon className="h-16 w-16" />
+            <h1 className="text-4xl font-bold ml-4">
+            Better Notion
+            </h1>
+          </div>
+          <div className="flex flex-col text-center justify-center align-center">
+            <h2 className="text-2xl font-semibold">Fast, Efficient and Productive</h2>
+            <h4 className="text-xl">
+          Empower your vision, ignite collaboration, and conquer every milestone
+            </h4>
+          </div>
+        </div>
+      </div>
+
+      {/* Right - Form */}
       {children}
     </div>
   )
